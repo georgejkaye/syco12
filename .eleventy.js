41f45a173d07785a967393610a223a2e2b6cab3a
George Kaye <georgejkaye@gmail.com>
2024-01-15 23:12:44 +0000

    Add ignores to site

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const yaml = require("js-yaml")
 
 module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy("static");
+    eleventyConfig.ignores.add("LICENSE")
+    eleventyConfig.ignores.add("README.md")
     eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
     return {
         dir: {
