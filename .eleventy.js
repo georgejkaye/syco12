54f9524a3677e0adb844b7e9f2fb11d4a9fda8f7
George Kaye <georgejkaye@gmail.com>
2024-03-21 09:02:49 +0000

    Add schedule framework

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy("static");
     eleventyConfig.ignores.add("LICENSE")
     eleventyConfig.ignores.add("README.md")
+    eleventyConfig.addFilter("makeUppercase", (value) => value.toUpperCase());
     eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
     return {
         dir: {
