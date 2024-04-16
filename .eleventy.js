73fc49be5412f5a4ee42e311e84828592a63ae2f
George Kaye <georgejkaye@gmail.com>
2024-04-16 20:31:03 +0100

    Add pictures page

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,28 @@ module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy("static");
     eleventyConfig.ignores.add("LICENSE")
     eleventyConfig.ignores.add("README.md")
-    eleventyConfig.addFilter("makeUppercase", (value) => value.toUpperCase());
-    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+    eleventyConfig.addFilter("makeUppercase", (value) => value.toUpperCase())
+    eleventyConfig.addFilter("presenters", (authors) => authors.filter((author) => author.presenter))
+    eleventyConfig.addFilter("nameList", (items) => {
+        return items.reduce((acc, cur, i) => {
+            if(i == 0) {
+                return cur.name
+            } else if(i == items.length - 1) {
+                return `${acc} and ${cur.name}`
+            } else {
+                return `${acc}, `
+            }
+        }, "")
+    })
+    eleventyConfig.addFilter("listWithoutAnd", (items) =>
+    items.reduce((acc, cur, i) => {
+        if(i == 0) {
+            return cur
+        } else {
+            return `${acc}, `
+        }
+    }))
+    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents))
     return {
         dir: {
             data: "data"
